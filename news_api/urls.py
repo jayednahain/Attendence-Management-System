1653706d491e16fa1e4c76a8254777f5fb2e24a7
jayednahian <jayednahian@yahoo.com>
2021-04-04 21:56:47 +0600

    first commit

--- news_api/urls.py
+++ news_api/urls.py
@@ -5,5 +5,7 @@ from .import views
 
 urlpatterns = [
 
-    path('scarpnews/',views.scarp_news, name="scarp_news_link")
+    path('scarpnews/',views.scarp_news, name="scarp_news_link"),
+    path('tribunenews/',views.trebune_news, name="tribune_news_link"),
+
 ]
